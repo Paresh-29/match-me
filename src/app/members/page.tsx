00453de4445b1ae4cdb7faaa00474cc0a3dc377f
Paresh-29 <pareshkumarbarick99@gmail.com>
2024-11-20 17:04:38 +0530

    Add age calculation utility, update database schema, and seed member data

--- src/app/members/page.tsx
+++ src/app/members/page.tsx
@@ -1,11 +1,5 @@
-import Link from "next/link";
+import React from "react";
 
-
-export default function MembersPage() {
-    return <div>
-        <h3 className="tetxt-3xl">
-            This Will be the Members Page
-        </h3>
-        <Link href="/">Go back home</Link>
-    </div>
+export default function page() {
+  return <div>This is members page</div>;
 }
